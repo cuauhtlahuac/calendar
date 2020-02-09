cf9dee95f0b82c658154ee1a12de21579d0b3a60
Rodrigo <cuitlagua@gmail.com>
2020-02-08 20:03:58 -0600

    header added

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './App.css';
+import Calendar from './components/Calendar';
 
 function App() {
-  return (
-    <div className="App">
-      <p>starting calendar</p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Starting calendar</h1>
+			<main>
+				<Calendar />
+			</main>
+		</div>
+	);
 }
 
 export default App;
--- calendar/src/components/Calendar.jsx
+++ calendar/src/components/Calendar.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { format } from "date-fns"
 import PropTypes from 'prop-types'
 
 const Calendar = props => {
+    const [date, setDate] = useState(new Date)
+
+    function renderHeader() {
+        const dateFormat = "MMMM yyyy";
+        return (
+            <div className="header row flex-middle">
+                <div className="col col-start">
+                    <div className="icon" onClick={prevMonth}>
+                        chevron_left
+              </div>
+                </div>
+                <div className="col col-center">
+                    <span>
+                        {format(date, dateFormat)}
+                    </span>
+                </div>
+                <div className="col col-end" onClick={nextMonth}>
+                    <div className="icon">chevron_right</div>
+                </div>
+            </div>
+        );
+    }
+
+    function renderDays() { }
+
+    function renderCells() { }
+
+    const onDateClick = day => { };
+
+    const nextMonth = () => { };
+
+    const prevMonth = () => { };
     return (
-        <div>
-            
+        <div style={{ backgroundColor: "black" }}>
+            <div>{renderHeader()}</div>
+            <div>{date.getDate()}</div>
+            <div>Cells</div>
         </div>
     )
 }
